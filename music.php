efdd89897ce3341d881c6447d7c20b508927837c
fflanagan89 <33397854+fflanagan89@users.noreply.github.com>
2017-11-28 14:19:19 -0500

    Update music.php

--- music.php
+++ music.php
@@ -1,6 +1,34 @@
 <?php
 include "top.php";
 ?>
+<body>
+
+.
+
+<center>
+
+<br><br>
+
+
+
+
+
+<br><br><br><br>
+
+<img src="images/apl1w2ks1qex.jpg" width="500px" />
+
+<br><br><br>
+  
+<b>Rap’s favorite weirdo delivers a stellar project with clever lyricism and production inspired by 80’s post-punk.
+</b>
+<p>What’s most impressive about Atrocity Exhibition is the energy Brown carries throughout the album. “When it Rain” has the intensity of Wu-Tang classic “Protect Ya Neck”, but instead of being helmed by seven rappers, Brown stands alone. This solo mentality gives Brown a unique edge. He’s the sole rapper on fourteen of the album’s fifteen tracks, a far cry from today’s typical feature heavy rap album.
+
+	Atrocity Exhibition excels where other rap albums fail. With the first lines of the opening track, “I’m sweating like I’m in a rave / Been in this room for three days”, Brown establishes a sense of tension and atmosphere unlike any modern rap album. The atmospheric nature continues throughout, exploring Brown’s “Downward Spiral” with lyrics both haunting and humorous.  
+
+	References on the album extend far beyond the realm of rap. By borrowing the album’s title from the Joy Division song of the same name, listeners can observe striking similarities between the late Ian Curtis’s troubled lyrics and Brown’s. Keeping in with the 70s/80s inspiration, tracks “Ain’t it Funny”, “Rolling Stone”, and “Dance in the Water” tap into the synth heavy, and often frantic, sound of New Order and Talking Heads.
+
+Posse cut “Really Doe”, the only song featuring verses from other rappers, is very fun and loosens up the tension built in the album's other tracks while maintaining Brown’s vision. This track is similar to those on his debut LP XXX in that they explore his Detroit origins. Fans may find themselves wishing Brown had a few more songs like “Really Doe” to break up the deep introspection that appears on nearly every track on the album. Ultimately, Atrocity Exhibition solidifies Danny Brown as one of the most forward thinking artists in the genre, molding his various inspirations into one very experimental package.</p>
+  
 <?php include "footer.php";?>
 </body>
 </html>
